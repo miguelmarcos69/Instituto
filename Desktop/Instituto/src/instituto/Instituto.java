370896670e1585e5e319db6a5f1abd3755ad9858
alumno <alumno@DESKTOP-J66QC1P>
2021-05-26 10:02:29 +0200

    Cambio a hola

--- Desktop/Instituto/src/instituto/Instituto.java
+++ Desktop/Instituto/src/instituto/Instituto.java
@@ -16,7 +16,7 @@ public class Instituto {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Pito2");
+        System.out.println("Hola ");
     }
     
 }
