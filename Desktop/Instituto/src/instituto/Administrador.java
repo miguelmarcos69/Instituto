d0fd6dffb8e84ab978fda8aa5cd51ec343b46926
alumno <alumno@DESKTOP-J66QC1P>
2021-05-27 11:17:02 +0200

    Implementamos varias clases base

---
+++ Desktop/Instituto/src/instituto/Administrador.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Usuarios;
+
+import java.util.Date;
+
+/**
+ *
+ * @author alumno
+ */
+public class Administrador extends Usuario{
+    
+    public Administrador(String nombre, String DNI, Date fecha_nacimiento, int edad, String contrasenna) {
+        super(nombre, DNI, fecha_nacimiento, edad, contrasenna);
+    }
+    
+}
---
+++ Desktop/Instituto/src/instituto/Usuario.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Usuarios;
+
+import java.util.Date;
+
+/**
+ *
+ * @author alumno
+ */
+public abstract class Usuario {
+    //atributos de la clase
+    private String nombre;
+    private String DNI;
+    private Date fecha_nacimiento;
+    private int edad;
+    private String contrasenna;
+
+    
+    
+    
+    //metodo constructor
+    public Usuario(String nombre, String DNI, Date fecha_nacimiento, int edad, String contrasenna) {
+        this.nombre = nombre;
+        this.DNI = DNI;
+        this.fecha_nacimiento = fecha_nacimiento;
+        this.edad = edad;
+        this.contrasenna = contrasenna;
+    }
+    
+    
+    
+    //metodos getters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public Date getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getContrasenna() {
+        return contrasenna;
+    }
+     
+    
+    
+    
+    
+}
