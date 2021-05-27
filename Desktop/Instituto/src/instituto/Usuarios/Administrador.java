652a28978d38a01da232e1094cea709a3ffbb333
alumno <alumno@DESKTOP-J66QC1P>
2021-05-27 11:31:32 +0200

    Creamos un paquete con los usuarios par amejorar la organizacion

--- Desktop/Instituto/src/instituto/Usuarios/Administrador.java
+++ Desktop/Instituto/src/instituto/Usuarios/Administrador.java
@@ -1,19 +1,20 @@
+package instituto.Usuarios;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Usuarios;
-
+import instituto.Usuarios.Usuario;
 import java.util.Date;
 
 /**
  *
  * @author alumno
  */
-public class Alumno extends Usuario{
+public class Administrador extends Usuario{
     
-    public Alumno(String nombre, String DNI, Date fecha_nacimiento, int edad, String contrasenna) {
+    public Administrador(String nombre, String DNI, Date fecha_nacimiento, int edad, String contrasenna) {
         super(nombre, DNI, fecha_nacimiento, edad, contrasenna);
     }
     
--- Desktop/Instituto/src/instituto/Usuarios/Profesor.java
+++ Desktop/Instituto/src/instituto/Usuarios/Profesor.java
@@ -1,10 +1,12 @@
+package instituto.Usuarios;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Usuarios;
 
+import instituto.Usuarios.Usuario;
 import java.util.Date;
 
 /**
